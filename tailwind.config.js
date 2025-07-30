25df55ff239b662b1013b1cc1e9d91fe8412b44b
piyush.patel <piyush.patel@rws.com>
2025-07-30 19:31:01 +0530

    inint 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,12 @@ module.exports = {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
+ theme: {
+    extend: {
+      colors: {
+        bgDark: "#0f172a"
+      }
+    },
   },
   plugins: [],
 };
